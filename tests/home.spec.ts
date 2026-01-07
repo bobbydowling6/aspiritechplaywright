ae3f946933db4ce2e8d3de918030094e18ea21a2
bobbydowling6 <bobbydowling6@gmail.com>
2026-01-06 19:22:42 -0600

    Made some changes

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -3,13 +3,13 @@ import { test, expect } from '@playwright/test';
 
 test.describe("Home Page", () => {
   test.beforeEach(async ({page}) => {
-    await page.goto("/")
+    await page.goto("https://aspiritech.org")
   })
 
 
 test("Validate first section of home page", async ({page}) => {
   //check the title of the page
-  await expect(page).toHaveTitle("Aspiritech – Professional tech services powered by neurodiversity. ")
+  await expect(page).toHaveTitle("Welcome to Aspiritech » Aspiritech Neurodivergent QA Testers")
   await expect(page).toHaveURL("/")
   await expect(page.getByRole('heading', { name: 'Quality Assurance, Accessibility, and Data Services' })).toBeVisible();
   await expect(page.getByText('Excellence powered by a')).toBeVisible();
@@ -21,32 +21,32 @@ test("Validate the Our Service buttons", async ({page}) => {
   await expect(page.getByRole('link', { name: 'Accessibility' }).first()).toBeVisible()
   //check the accessibility button is functional and is redirected to the right Url
   await page.getByRole('link', { name: 'Accessibility' }).first().click();
-  await expect(page).toHaveTitle("Accessibility Testing: Design, UX, Usability, & WCAG Conformance – Aspiritech")
-  await expect(page).toHaveURL("/services/accessibility/")
-  await expect(page.getByRole('heading', { name: 'Accessibility Testing: Design' })).toBeVisible();
+  await expect(page).toHaveTitle("Accessibility Testing: Design, Usability, & WCAG Conformance » Aspiritech")
+  await expect(page).toHaveURL("https://aspiritech.org/services/accessibility/")
+  await expect(page.getByRole('heading', { name: 'Accessibility' })).toBeVisible();
   await page.goBack()
   //check the audio testing button is visible
   await expect(page.getByRole('link', { name: 'Audio Testing' }).first()).toBeVisible()
   //check the audio testing button is functional and is redirected to the right Url
   await page.getByRole('link', { name: 'Audio Testing' }).first().click();
-  await expect(page).toHaveTitle("Audio & Hardware Testing: Speakers, Headphones, & Connectivity – Aspiritech")
-  await expect(page).toHaveURL("/services/audio-testing/")
+  await expect(page).toHaveTitle("Audio & Hardware Testing: Speakers, Headphones, & Connectivity » Aspiritech")
+  await expect(page).toHaveURL("https://aspiritech.org/services/audio-testing/")
   await expect(page.getByRole('heading', { name: 'Audio & Hardware Testing: Speakers, Headphones, & Connectivity' })).toBeVisible();
   await page.goBack()
   //check the quality assurance button is visible
   await expect(page.getByRole('link', { name: 'Quality Assurance' }).first()).toBeVisible()
   //check the quality assurance button is functional and is redirected to the right Url
   await page.getByRole('link', { name: 'Quality Assurance' }).first().click();
-  await expect(page).toHaveTitle("Software Quality Assurance – Aspiritech")
-  await expect(page).toHaveURL("/services/quality-assurance/")
+  await expect(page).toHaveTitle("Software Quality Assurance » Aspiritech")
+  await expect(page).toHaveURL("https://aspiritech.org/services/quality-assurance/")
   await expect(page.getByRole('heading', { name: 'Software Quality Assurance' })).toBeVisible();
   await page.goBack()
   //check the data services button is visible
   await expect(page.getByRole('link', { name: 'Data Services' }).first()).toBeVisible()
   //check the data services button is functional and is redirected to the right Url
   await page.getByRole('link', { name: 'Data Services' }).first().click();
-  await expect(page).toHaveTitle("Data Services: Data Annotation, Data Labeling, and Data Validation – Aspiritech")
-  await expect(page).toHaveURL("/services/data-services/")
+  await expect(page).toHaveTitle("Data Services: Data Annotation, Data Labeling, and Data Validation » Aspiritech")
+  await expect(page).toHaveURL("https://aspiritech.org/services/data-services/")
   await expect(page.getByRole('heading', { name: 'Data Services: Data Annotation, Data Labeling, and Data Validation' })).toBeVisible();
   await page.goBack()
 })
@@ -56,36 +56,36 @@ test("Discover How We Can Help section title", async ({page}) => {
   await expect(page.getByRole('heading', { name: 'Discover How We Can Help' })).toBeVisible();
   //Validate link is functional
   await page.getByRole('link', { name: 'Quality Assurance, End-To-End' }).click();
-  await expect(page).toHaveTitle("Software Quality Assurance – Aspiritech")
-  await expect(page).toHaveURL("/services/quality-assurance/")
+  await expect(page).toHaveTitle("Software Quality Assurance » Aspiritech")
+  await expect(page).toHaveURL("https://aspiritech.org/services/quality-assurance/")
   await page.goBack()
 })
   test("Validate the Discover How We Can Help section including the All Services", async ({page}) => {
   //validate all services links are functional & are redirected to the right Url
   await expect(page.getByRole('heading', { name: 'All Services' })).toBeVisible()
   await page.locator('a').filter({ hasText: /^Audio Testing$/ }).click();
-  await expect(page).toHaveTitle("Audio & Hardware Testing: Speakers, Headphones, & Connectivity – Aspiritech")
-  await expect(page).toHaveURL("/services/audio-testing/")
+  await expect(page).toHaveTitle("Audio & Hardware Testing: Speakers, Headphones, & Connectivity » Aspiritech")
+  await expect(page).toHaveURL("https://aspiritech.org/services/audio-testing/")
   await expect(page.getByRole('heading', { name: 'Audio & Hardware Testing: Speakers, Headphones, & Connectivity' })).toBeVisible();
   await page.goBack()
   await page.getByRole('link', { name: 'Accessibility Testing &' }).click();
-  await expect(page).toHaveTitle("Accessibility Testing: Design, UX, Usability, & WCAG Conformance – Aspiritech")
-  await expect(page).toHaveURL("/services/accessibility/")
+  await expect(page).toHaveTitle("Accessibility Testing: Design, UX, Usability, & WCAG Conformance » Aspiritech")
+  await expect(page).toHaveURL("https://aspiritech.org/services/accessibility/")
   await expect(page.getByRole('heading', { name: 'Accessibility Testing: Design' })).toBeVisible();
   await page.goBack()
   await page.getByRole('link', { name: 'Software Quality Assurance' }).click();
-  await expect(page).toHaveTitle("Software Quality Assurance – Aspiritech")
-  await expect(page).toHaveURL("/services/quality-assurance/")
+  await expect(page).toHaveTitle("Software Quality Assurance » Aspiritech")
+  await expect(page).toHaveURL("https://aspiritech.org/services/quality-assurance/")
   await expect(page.getByRole('heading', { name: 'Software Quality Assurance' })).toBeVisible();
   await page.goBack()
   await page.getByRole('listitem').filter({ hasText: /^Data Services$/ }).getByRole('link').click();
-  await expect(page).toHaveTitle("Data Services: Data Annotation, Data Labeling, and Data Validation – Aspiritech")
-  await expect(page).toHaveURL("/services/data-services/")
+  await expect(page).toHaveTitle("Data Services: Data Annotation, Data Labeling, and Data Validation » Aspiritech")
+  await expect(page).toHaveURL("https://aspiritech.org/services/data-services/")
   await expect(page.getByRole('heading', { name: 'Data Services: Data Annotation, Data Labeling, and Data Validation' })).toBeVisible();
   await page.goBack()
   await page.getByRole('link', { name: 'Web Design & Development' }).click();
-  await expect(page).toHaveTitle("Web Design & Development – Aspiritech")
-  await expect(page).toHaveURL("/services/web-design-development/")
+  await expect(page).toHaveTitle("Web Design & Development » Aspiritech")
+  await expect(page).toHaveURL("https://aspiritech.org/services/web-design-development/")
   await expect(page.getByRole('heading', { name: 'Web Design & Development' })).toBeVisible();
   await page.goBack()
 })
@@ -96,8 +96,8 @@ test("Discover How We Can Help section title", async ({page}) => {
 test("Validate the Our Expertise Section", async ({page}) => { 
   await expect(page.locator('section').filter({ hasText: 'Our Expertise Our tech' }).getByRole('heading')).toBeVisible();
   await page.getByRole('link', { name: 'Services', exact: true }).click();
-  await expect(page).toHaveTitle("Services – Aspiritech")
-  await expect(page).toHaveURL("/services/")
+  await expect(page).toHaveTitle("Services » Aspiritech Aspiritech Offers QA Services.")
+  await expect(page).toHaveURL("https://aspiritech.org/services/")
   await expect(page.getByRole('heading', { name: 'Services' , exact: true})).toBeVisible();
   await page.goBack();
 })
@@ -113,8 +113,8 @@ test("Validate the Aspiritech Video playback", async ({page}) => {
 test("Validate the Our Team Section", async ({page}) => {
   await expect(page.getByRole('heading', { name: 'Our Team' })).toBeVisible();
   await page.getByRole('link', { name: 'About Aspiritech' }).click();
-  await expect(page).toHaveTitle("About Us – Aspiritech")
-  await expect(page).toHaveURL("/about-us/")
+  await expect(page).toHaveTitle("About Us » Aspiritech")
+  await expect(page).toHaveURL("https://aspiritech.org/about-us/")
   await expect(page.getByRole('heading', { name: 'About Us' })).toBeVisible();
   await page.goBack()
 })
@@ -122,8 +122,8 @@ test("Validate the Our Team Section", async ({page}) => {
 test("Validate the Our Philosophy Section", async ({page}) => {
   await expect(page.getByRole('heading', { name: 'Our Philosophy' })).toBeVisible();
   await page.getByRole('link', { name: 'Our Story' }).nth(1).click();
-  await expect(page).toHaveTitle("Our Story – Aspiritech")
-  await expect(page).toHaveURL("/our-story/")
+  await expect(page).toHaveTitle("Our Story » Aspiritech")
+  await expect(page).toHaveURL("https://aspiritech.org/our-story/")
   await expect(page.getByRole('heading', { name: 'Our Story' })).toBeVisible();
   await page.goBack()
   
